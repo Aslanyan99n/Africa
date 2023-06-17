19b00921e80e88b120653eacb5c9f5d3fb554ea4
Aslanyan99n <a99narek@gmail.com>
2023-06-17 11:10:51 +0400

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Narek on 17.06.23.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
